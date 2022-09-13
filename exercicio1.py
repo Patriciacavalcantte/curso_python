d7bbb1a501ba84ea7d66fa744cef40051e26a1cc
Patrícia Cavalcante <patriciacavalcante@gmail.com>
2022-09-13 18:28:20 -0300

    curso de Python

---
+++ exercicio1.py
@@ -0,0 +1,18 @@
+#concatenar: ("o valor digitado foi:  + algo ")
+#======= Tipos de dados mais utilizados =======
+#str "222vfbvbafad#@55523!"
+#int 123456
+#float 2.5 7.9 3.0 
+#bool true false 
+
+# Dado que o prof Danilo, precisa mostrar algumas informações importantes na tela 
+#faça um programa que capture essas informações e mostre na tela de forma organizada:
+#exemplo de organização: 
+#Programa de formatação de dados 
+#Nome: XXXXXX
+# Telefone: XXXXXX
+# Endereço: XXXXXXX
+
+print("Digite seu nome, telefone e endereço") 
+str.informacoes=input()
+print("Os dados digitados foram: " + informacoes)
