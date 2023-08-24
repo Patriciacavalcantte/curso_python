36edb09951d1168bda78f8343c224064716d31ff
Patrícia Cavalcante <patriciacavalcante@gmail.com>
2023-08-24 15:00:10 -0300

    Jogo da palavra secreta em Python

--- for_in.py
+++ for_in.py
@@ -9,7 +9,24 @@
 For + Range
 range -> range(start, stop, step)
 """
-numeros = range(0, 100, 2)
+'''numeros = range(0, 100, 2)
 
 for numero in numeros:
-    print(numero)
+    print(numero)'''
+
+'''texto = iter('Patricia') #_iter_()
+print(next(texto)) #itera e mostra na tela as letras da string'''
+
+for i in range(10):
+    if i == 2:
+        print('i é 2, pulando...')
+        continue
+
+    if i == 8:
+        print('i é 8, seu else não executará')
+        break
+
+    for j in range(1, 3):
+        print(i, j)
+else:
+    print('For completo com sucesso!')
